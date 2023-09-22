225d2da7cc066a9da10a0c3d0bb95d70dc4469ca
Miguel Negron Garcia <josuefrontend@gmail.com>
2023-09-21 20:04:07 -0500

    Still 5.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   darkMode: 'class',
-  content: ["./src/index.html"],
+  content: ["./index.html"],
   theme: {
     extend: {
       backgroundImage : {
